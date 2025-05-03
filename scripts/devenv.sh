c26fc470bb43864fcb2d6e2160d6da995cc56237
HexagramNM <hexagram_n.m@jcom.home.ne.jp>
2025-05-03 13:03:45 +0000

    Add os-all-build alias

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -10,6 +10,7 @@ alias cd-mikanos="cd $MIKANOS_ROOT"
 alias kernel-build="cd $MIKANOS_ROOT/kernel; clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding -c main.cpp"
 alias kernel-link="cd $MIKANOS_ROOT/kernel; ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o"
 alias loader-build="cd $HOME/edk2; source edksetup.sh; build;"
+alias os-all-build="cd $MIKANOS_ROOT/kernel; make -B; cd $HOME/edk2; source edksetup.sh; build;"
 alias run-qemu="cd $HOME/edk2; $HOME/osbook/devenv/run_qemu.sh $HOME/edk2/Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $MIKANOS_ROOT/kernel/kernel.elf"
 
 alias reload-devenv="source $MIKANOS_ROOT/scripts/devenv.sh"
