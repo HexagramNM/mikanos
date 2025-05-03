32345f50b499efe164e6744b9f59b97a0be53df1
penpenpng <poman1638m@gmail.com>
2025-05-03 08:01:09 +0000

    update kernel to draw something
    Co-authored-by: Genroh <Genroh@users.noreply.github.com>
    Co-authored-by: sasami312 <sasami312@users.noreply.github.com>
    Co-authored-by: HexagramNM <HexagramNM@users.noreply.github.com>

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -1,3 +1,8 @@
 # Usage: source devenv.sh
 
 export PATH=$PATH:$HOME/osbook/devenv
+
+source $HOME/osbook/devenv/buildenv.sh
+
+alias kernel-build="clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding -c main.cpp"
+alias kernel-link="ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o"
