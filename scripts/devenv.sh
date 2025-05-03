9c62f512475ce6b98bef1500eda4b2711ec7cec5
penpenpng <poman1638m@gmail.com>
2025-05-03 08:19:46 +0000

    add dev utils

--- scripts/devenv.sh
+++ scripts/devenv.sh
@@ -5,6 +5,9 @@ export MIKANOS_ROOT=$(cd $(dirname ${BASH_SOURCE:-$0}); cd ..; pwd)
 
 source $HOME/osbook/devenv/buildenv.sh
 
+alias cd-devenv="cd $HOME/osbook/devenv"
+alias cd-mikanos="cd $MIKANOS_ROOT"
 alias kernel-build="cd $MIKANOS_ROOT/kernel; clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding -c main.cpp"
 alias kernel-link="cd $MIKANOS_ROOT/kernel; ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o"
+alias loader-build="cd $HOME/osbook/devenv; source edksetup.sh; build;"
 alias run-qemu="$HOME/osbook/devenv/run_qemu.sh Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi $MIKANOS_ROOT/kernel/kernel.elf"
