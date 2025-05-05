58fcd925a3587951fb354d92d9c0c531809058b4
penpenpng <poman1638m@gmail.com>
2025-05-05 04:34:08 +0000

    pass memory map to kernel
    Co-authored-by: HexagramNM <HexagramNM@users.noreply.github.com>
    Co-authored-by: sasami312 <sasami312@users.noreply.github.com>
    Co-authored-by: Genroh <Genroh@users.noreply.github.com>

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -20,6 +20,7 @@
 #include "interrupt.hpp"
 #include "asmfunc.h"
 #include "queue.hpp"
+#include "memory_map.hpp"
 
 const PixelColor kDesktopBGColor{45, 118, 237};
 const PixelColor kDesktopFGColor{255, 255, 255};
@@ -95,7 +96,8 @@ __attribute__((interrupt)) void IntHandlerXHCI(InterruptFrame *frame)
     NotifyEndOfInterrupt();
 }
 
-extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
+extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config,
+                           const MemoryMap &memory_map)
 {
     switch (frame_buffer_config.pixel_format)
     {
@@ -132,6 +134,32 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
     console = new (console_buf) Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
     printk("Welcome to MikanOS!\n");
     SetLogLevel(kWarn);
+
+    const std::array available_memory_types{
+        MemoryType::kEfiBootServicesCode,
+        MemoryType::kEfiBootServicesData,
+        MemoryType::kEfiConventionalMemory,
+    };
+
+    printk("memory_map: %p\n", &memory_map);
+    for (uintptr_t iter = reinterpret_cast<uintptr_t>(memory_map.buffer);
+         iter < reinterpret_cast<uintptr_t>(memory_map.buffer) + memory_map.map_size;
+         iter += memory_map.descriptor_size)
+    {
+        auto desc = reinterpret_cast<MemoryDescriptor *>(iter);
+        for (int i = 0; i < available_memory_types.size(); ++i)
+        {
+            if (desc->type == available_memory_types[i])
+            {
+                printk("type = %u, phys = %08lx - %08lx, pages = %lu, attr = %08lx\n",
+                       desc->type,
+                       desc->physical_start,
+                       desc->physical_start + desc->number_of_pages * 4096 - 1,
+                       desc->number_of_pages,
+                       desc->attribute);
+            }
+        }
+    }
 
     mouse_cursor = new (mouse_cursor_buf) MouseCursor{
         pixel_writer, kDesktopBGColor, {300, 200}};
