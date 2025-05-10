5aa8dd361d1b673657d66edc14ead938434544e5
penpenpng <poman1638m@gmail.com>
2025-05-10 09:53:00 +0000

    register keyboard intrrupt handler
    Co-authored-by: HexagramNM <HexagramNM@users.noreply.github.com>
    Co-authored-by: sasami312 <sasami312@users.noreply.github.com>
    Co-authored-by: Genroh <Genroh@users.noreply.github.com>

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -26,6 +26,7 @@
 #include "timer.hpp"
 #include "message.hpp"
 #include "acpi.hpp"
+#include "keyboard.hpp"
 
 #ifndef MIKANOS_VERSION
 #define MIKANOS_VERSION "null"
@@ -95,8 +96,7 @@ extern "C" void KernelMainNewStack(
     acpi::Initialize(acpi_table);
     InitializeLAPICTimer(*main_queue);
 
-    timer_manager->AddTimer(Timer(200, 2));
-    timer_manager->AddTimer(Timer(600, -1));
+    InitializeKeyboard(*main_queue);
 
     char str[128];
     printk("Welcome to MikanOS! v-%s\n", MIKANOS_VERSION);
@@ -129,12 +129,11 @@ extern "C" void KernelMainNewStack(
             usb::xhci::ProcessEvents();
             break;
         case Message::kTimerTimeout:
-            printk("Timer: timeout = %lu, value = %d\n",
-                   msg.arg.timer.timeout, msg.arg.timer.value);
-            if (msg.arg.timer.value > 0)
+            break;
+        case Message::kKeyPush:
+            if (msg.arg.keyboard.ascii != 0)
             {
-                timer_manager->AddTimer(Timer(
-                    msg.arg.timer.timeout + 100, msg.arg.timer.value + 1));
+                printk("%c", msg.arg.keyboard.ascii);
             }
             break;
         default:
