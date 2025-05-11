4abd3344c9ae9d41a06fd5a0becf53634112854a
penpenpng <poman1638m@gmail.com>
2025-05-11 03:50:35 +0000

    implement SwitchContext()
    Co-authored-by: HexagramNM <HexagramNM@users.noreply.github.com>
    Co-authored-by: sasami312 <sasami312@users.noreply.github.com>
    Co-authored-by: Genroh <Genroh@users.noreply.github.com>

--- kernel/asmfunc.asm
+++ kernel/asmfunc.asm
@@ -76,6 +76,11 @@ SetCR3:
     mov cr3, rdi
     ret
 
+global GetCR3  ; uint64_t GetCR3();
+GetCR3:
+    mov rax, cr3
+    ret
+
 extern kernel_main_stack
 extern KernelMainNewStack
 
@@ -86,3 +91,104 @@ KernelMain:
 .fin:
     hlt
     jmp .fin
+
+global SwitchContext
+SwitchContext:  ; void SwitchContext(void* next_ctx, void* current_ctx);
+    mov [rsi + 0x40], rax ; current_ctx.rax = rax
+    mov [rsi + 0x48], rbx ; ditto
+    mov [rsi + 0x50], rcx
+    mov [rsi + 0x58], rdx
+    mov [rsi + 0x60], rdi
+    mov [rsi + 0x68], rsi
+
+    ; SwitchContext() が呼ばれた時点で rsp のポインタはずれてしまっているので、
+    ; 本来保存したいスタックの位置は計算する必要がある (rsp + 8)。
+    ; current_ctx.rsp = [rsp + 8]
+    lea rax, [rsp + 8]
+    mov [rsi + 0x70], rax
+
+    ; current_ctx.rbp = rbp
+    mov [rsi + 0x78], rbp
+
+    mov [rsi + 0x80], r8
+    mov [rsi + 0x88], r9
+    mov [rsi + 0x90], r10
+    mov [rsi + 0x98], r11
+    mov [rsi + 0xa0], r12
+    mov [rsi + 0xa8], r13
+    mov [rsi + 0xb0], r14
+    mov [rsi + 0xb8], r15
+
+    ; current_ctx.cr3 = cr3
+    mov rax, cr3
+    mov [rsi + 0x00], rax
+
+    ; rsp の指す先は rip なので、current_ctx.rip = [rsp]
+    mov rax, [rsp]
+    mov [rsi + 0x08], rax
+
+    ; mov は rflags に対して使えないので、フラグレジスタ rflags の内容を一旦スタックに積む。
+    pushfq
+    ; すぐ取り出す。 current_ctx.rflags = rflags
+    pop qword [rsi + 0x10]
+
+    ; cs の 16bit を (セグメントレジスタは 16 bit 幅しかない) current_ctx.cs に格納する。
+    mov ax, cs
+    mov [rsi + 0x20], rax
+
+    ; 以下同様。
+    mov bx, ss
+    mov [rsi + 0x28], rbx
+    mov cx, fs
+    mov [rsi + 0x30], rcx
+    mov dx, gs
+    mov [rsi + 0x38], rdx
+
+    ; fxsave は指定された位置に x87 FPU, MMX technology, XMM, MXCSR レジスタの内容を書き出す命令。
+    ; 要するに current_ctx.fxsave = fxsave みたいなもん。
+    fxsave [rsi + 0xc0]
+
+    ; iret (interruption return) 命令によって next_ctx の内容をレジスタに書き戻すため、
+    ; next_ctx の内容をスタックに積む。
+    push qword [rdi + 0x28] ; SS
+    push qword [rdi + 0x70] ; RSP
+    push qword [rdi + 0x10] ; RFLAGS
+    push qword [rdi + 0x20] ; CS
+    push qword [rdi + 0x08] ; RIP
+
+    ; fx restore
+    fxrstor [rdi + 0xc0]
+
+    ; cr3 = next_ctx.cr3
+    mov rax, [rdi + 0x00]
+    mov cr3, rax
+
+    ; fs = next_ctx.fs
+    mov rax, [rdi + 0x30]
+    mov fs, ax
+
+    ; gs = next_ctx.gs
+    mov rax, [rdi + 0x38]
+    mov gs, ax
+
+    mov rax, [rdi + 0x40]
+    mov rbx, [rdi + 0x48]
+    mov rcx, [rdi + 0x50]
+    mov rdx, [rdi + 0x58]
+    mov rsi, [rdi + 0x68]
+    mov rbp, [rdi + 0x78]
+    mov r8,  [rdi + 0x80]
+    mov r9,  [rdi + 0x88]
+    mov r10, [rdi + 0x90]
+    mov r11, [rdi + 0x98]
+    mov r12, [rdi + 0xa0]
+    mov r13, [rdi + 0xa8]
+    mov r14, [rdi + 0xb0]
+    mov r15, [rdi + 0xb8]
+
+    ; rdi はここまでの書き戻しの処理でずっと使っていたので、最後に書き戻す。
+    mov rdi, [rdi + 0x60]
+
+    ; iret 命令によってスタックに積んだ内容をレジスタに書き戻している。
+    ; o64 iret はたぶん iretq と同じもの。
+    o64 iret
