86ca3d75f7a26b81e429a8ab945b87c071628de4
penpenpng <poman1638m@gmail.com>
2025-05-11 01:54:52 +0000

    blink cursor
    Co-authored-by: HexagramNM <HexagramNM@users.noreply.github.com>
    Co-authored-by: sasami312 <sasami312@users.noreply.github.com>
    Co-authored-by: Genroh <Genroh@users.noreply.github.com>

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -84,6 +84,14 @@ void InitializeTextWindow()
 }
 
 int text_window_index;
+
+void DrawTextCursor(bool visible)
+{
+    const auto color = visible ? ToColor(0) : ToColor(0xffffff);
+    const auto pos = Vector2D<int>{8 + 8 * text_window_index, 24 + 5};
+    FillRectangle(*text_window->Writer(), pos, {7, 15}, color);
+}
+
 void InputTextWindow(char c)
 {
     if (c == 0)
@@ -94,17 +102,20 @@ void InputTextWindow(char c)
     auto pos = []()
     { return Vector2D<int>{8 + 8 * text_window_index, 24 + 6}; };
 
-    const int max_chars = (text_window->Width() - 16) / 8;
+    const int max_chars = (text_window->Width() - 16) / 8 - 1;
     if (c == '\b' && text_window_index > 0)
     {
+        DrawTextCursor(false);
         --text_window_index;
         FillRectangle(*text_window->Writer(), pos(), {8, 16}, ToColor(0xffffff));
+        DrawTextCursor(true);
     }
     else if (c >= ' ' && text_window_index < max_chars)
-
     {
+        DrawTextCursor(false);
         WriteAscii(*text_window->Writer(), pos(), c, ToColor(0));
         ++text_window_index;
+        DrawTextCursor(true);
     }
 
     layer_manager->Draw(text_window_layer_id);
@@ -136,14 +147,21 @@ extern "C" void KernelMainNewStack(
 
     InitializeLayer();
     InitializeMainWindow();
-    InitializeMouse();
     InitializeTextWindow();
+    InitializeMouse();
     layer_manager->Draw({{0, 0}, ScreenSize()});
 
     acpi::Initialize(acpi_table);
     InitializeLAPICTimer(*main_queue);
 
     InitializeKeyboard(*main_queue);
+
+    const int kTextboxCursorTimer = 1;
+    const int kTimer05Sec = static_cast<int>(kTimerFreq * 0.5);
+    __asm__("cli");
+    timer_manager->AddTimer(Timer{kTimer05Sec, kTextboxCursorTimer});
+    __asm__("sti");
+    bool textbox_cursor_visible = false;
 
     char str[128];
     printk("Welcome to MikanOS! v-%s\n", MIKANOS_VERSION);
@@ -176,6 +194,16 @@ extern "C" void KernelMainNewStack(
             usb::xhci::ProcessEvents();
             break;
         case Message::kTimerTimeout:
+            if (msg.arg.timer.value == kTextboxCursorTimer)
+            {
+                __asm__("cli");
+                timer_manager->AddTimer(
+                    Timer{msg.arg.timer.timeout + kTimer05Sec, kTextboxCursorTimer});
+                __asm__("sti");
+                textbox_cursor_visible = !textbox_cursor_visible;
+                DrawTextCursor(textbox_cursor_visible);
+                layer_manager->Draw(text_window_layer_id);
+            }
             break;
         case Message::kKeyPush:
             InputTextWindow(msg.arg.keyboard.ascii);
